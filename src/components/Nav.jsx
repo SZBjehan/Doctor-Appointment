43316842feb5f430f6fc516ac68cd543ab53873a
Syed Ziaul Bin Bashar <ziaulbinbashar@gmail.com>
2022-08-23 17:36:29 +0600

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import sqhLogo from '../square-health.png'
+import {Link} from 'react-router-dom'; 
+
 function Nav(){
 
     return (
         <div className='nav' id='Nav'>
-            <div class="nav-logo">
-                <a href="/"><img src={sqhLogo} alt="Logo" class="nav-photo" /></a>
+            <div className='nav-logo'>
+                <Link 
+                    to={{
+                        pathname: "/"
+                    }}
+                >
+                
+                    <img src={sqhLogo} alt="Logo" class="nav-photo" />
+                </Link>
             </div>
             {/* <div class="nav-title">
                 Doctors Appointment
